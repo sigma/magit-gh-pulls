f5709237d8281746475f7f2c55fd78743e37ce1e
Yann Hodique <yann.hodique@gmail.com>
2011-11-26 03:59:05 +0100

    use caching
    also fix metadata

--- magit-gh-pulls.el
+++ magit-gh-pulls.el
@@ -2,18 +2,25 @@
 
 ;; Copyright (C) 2011  Yann Hodique
 
-;; Magit is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
+;; Author: Yann Hodique <yann.hodique@gmail.com>
+;; Keywords:
+;; Version: 0.2
+;; Package-Requires: ((gh "0.4.0"))
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
-;;
-;; Magit is distributed in the hope that it will be useful, but WITHOUT
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-;; License for more details.
-;;
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
 ;; You should have received a copy of the GNU General Public License
-;; along with Magit.  If not, see <http://www.gnu.org/licenses/>.
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
 
@@ -34,7 +41,7 @@
 (magit-define-inserter gh-pulls ()
   (magit-with-section "Pull Requests" 'pulls
     (insert (propertize "Pull Requests:\n" 'face 'magit-section-title))
-    (let* ((api (gh-pulls-api "api" :sync t))
+    (let* ((api (gh-pulls-api "api" :sync t :cache t))
            (repo (magit-gh-pulls-guess-repo))
            (reqs (oref (gh-pulls-list api (car repo) (cdr repo)) :data)))
       (dolist (req reqs)
